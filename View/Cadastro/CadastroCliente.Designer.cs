5d6a207ff8a625cd78bfe186e97ef201a82b174f
Douglas Porto <douglaswilliamssp@gmail.com>
2026-06-27 18:29:32 -0300

    Feat: Criado um novo componente tela de cadastro do cliente

--- View/Cadastro/CadastroCliente.Designer.cs
+++ View/Cadastro/CadastroCliente.Designer.cs
@@ -1,14 +1,14 @@
-﻿namespace Sistema_Caixa_Eletronico
+﻿namespace Sistema_Caixa_Eletronico.View.Cadastro
 {
-    partial class LoginForm
+    partial class CadastroCliente
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,20 +23,20 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
             SuspendLayout();
             // 
-            // LoginForm
+            // CadastroCliente
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(554, 338);
-            Name = "LoginForm";
-            Text = "Login";
+            ClientSize = new Size(800, 450);
+            Name = "CadastroCliente";
+            Text = "Cadastro";
             ResumeLayout(false);
         }
 
