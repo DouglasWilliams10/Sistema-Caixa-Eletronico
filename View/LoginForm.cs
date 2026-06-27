97342eee6807072cec253078dc7a7f8b900dc588
Douglas Porto <douglaswilliamssp@gmail.com>
2026-06-27 18:17:45 -0300

    Adicionar arquivos de projeto.

---
+++ View/LoginForm.cs
@@ -0,0 +1,10 @@
+namespace Sistema_Caixa_Eletronico
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
